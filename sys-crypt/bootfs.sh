707606931601cb9f380df5471cfdc0c674bc945b
Alex Suykov <alex.suykov@gmail.com>
2017-11-02 12:04:44 +0200

    EFI32 syslinux files
    No idea whether it will work, but who knows.
    The kernel should tolerate mixed-mode boot.

--- sys-crypt/bootfs.sh
+++ sys-crypt/bootfs.sh
@@ -17,4 +17,6 @@ mmd -i bootfs.img ::/EFI/BOOT
 mcopy -i bootfs.img bootfs/* ::
 mcopy -i bootfs.img /usr/lib/syslinux/efi64/syslinux.efi ::/EFI/BOOT/BOOTX64.EFI
 mcopy -i bootfs.img /usr/lib/syslinux/efi64/ldlinux.e64  ::/EFI/BOOT/ldlinux.e64
+mcopy -i bootfs.img /usr/lib/syslinux/efi32/syslinux.efi ::/EFI/BOOT/BOOTAI32.EFI
+mcopy -i bootfs.img /usr/lib/syslinux/efi32/ldlinux.e32  ::/EFI/BOOT/ldlinux.e32
 syslinux -i bootfs.img
